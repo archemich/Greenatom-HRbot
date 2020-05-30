379ca5947e3de8365c9e6063aa9bc825bd79d365
archemich <archemich@gmail.com>
2020-05-31 01:02:23 +0300

    added Category, upgraded interface for questions and categories

--- webinterface/tests/admin.py
+++ webinterface/tests/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
-# Register your models here.
-from .models import Question, PossibleAnswer
+from .models import Question, PossibleAnswer, Category
+
 
 class PossibleAnswerInline(admin.StackedInline):
-	model = PossibleAnswer
-	extra = 4
+    model = PossibleAnswer
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
-	inlines = [PossibleAnswerInline]
-	list_display = ('question_text', 'category', 'difficulty')
-	list_filter = ('category', 'difficulty')
-	search_fields = ['question_text']
+    inlines = [PossibleAnswerInline]
+    list_display = ('question_text', 'category', 'difficulty', 'isOn')
+    list_filter = ('category', 'difficulty')
+    search_fields = ['question_text', 'category']
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    Question.isOn.short_description = 'Включен?'
+    list_display = ('category', 'isOn')
+    list_editable = ('isOn',)
+    list_filter = ('isOn',)
+    search_fields = ('category',)
 
-
-
-
-
-
-
-admin.site.register(Question,QuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Category, CategoryAdmin)
--- webinterface/tests/models.py
+++ webinterface/tests/models.py
@@ -2,48 +2,66 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
 
+    category = models.CharField('Категория', max_length=100)
+    isOn = models.BooleanField('Включить категорию', default=True,
+                               null=False, blank=False)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['-isOn', 'category']
 
 
 class Question (models.Model):
-	
-	question_text = models.TextField('Текст вопроса')
 
-	true_answer = models.CharField('Правильный ответ', max_length = 50, default = 'Введите ответ', null = False, blank = False)
+    question_text = models.TextField('Текст вопроса', max_length=500)
 
+    true_answer = models.CharField('Правильный ответ', max_length=50,
+                                   default='Введите ответ', null=False,
+                                   blank=False)
 
-	DIFFICULTY = [
-	    ('Easy','Easy'),
-	    ('Medium', 'Medium'),
-	    ('Hard', 'Hard'),
-	]
-	difficulty = models.CharField('Сложность', max_length = 50,
-		choices = DIFFICULTY, default = 'Выберите сложность')
+    DIFFICULTY = [
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Hard', 'Hard'),
+    ]
+    difficulty = models.CharField('Сложность', max_length=50,
+                                  choices=DIFFICULTY,
+                                  default='Выберите сложность')
 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 verbose_name='Категория')
 
-	CATEGORY = [
-		('Java', 'Java'),
-		('C++', 'C++'),
-	]
+    def isOn(self):
+        a = self.category.isOn
+        short_description = 'Включен'
+        if a is True:
+            return 'Да'
+        else:
+            return 'Нет'
 
-	category = models.CharField('Категория', max_length = 100, 
-		choices = CATEGORY, default = 'Выберите тему')
+    def __str__(self):
+        return self.question_text[:20]
 
-
-	class Meta:
-		verbose_name = 'Вопрос'
-		verbose_name_plural = 'Вопросы'
-
-
-
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
 
 
 class PossibleAnswer (models.Model):
-	question = models.ForeignKey(Question,
-		on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                 verbose_name='Вопрос')
 
-	answer = models.CharField('Ответ', max_length = 50)
+    answer = models.CharField('Ответ', max_length=50)
 
-	class Meta:
-		verbose_name = 'Ответ'
-		verbose_name_plural = 'Ответы'
+    def __str__(self):
+        return self.answer
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
