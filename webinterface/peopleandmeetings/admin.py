e3801a7bd68290825d3ff63d11581ec2afacbb21
archemich <archemich@gmail.com>
2020-05-31 02:44:29 +0300

    peopleandmeetings app upgrade

--- webinterface/peopleandmeetings/admin.py
+++ webinterface/peopleandmeetings/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Person, Meeting
-# Register your models here.
 
-admin.site.register(Person)
-admin.site.register(Meeting)
+
+# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('telegram_id', 'tests_score',
+                    'competition_score', 'get_prefered_categories')
+    search_fields = ['telegram_id']
+
+    def get_prefered_categories(self, obj):
+        return ",\n".join([p.category for p in obj.prefered_categories.all()])
+
+
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ('meeting_name', 'date')
+    search_fields = ['meeting_name']
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Meeting, MeetingAdmin)
--- webinterface/peopleandmeetings/models.py
+++ webinterface/peopleandmeetings/models.py
@@ -1,28 +1,35 @@
+from tests.models import Category
 from django.db import models
 
+
 # Create your models here.
-
 class Meeting(models.Model):
-	meeting_name = models.CharField('Название встречи', max_length = 100)
+    meeting_name = models.CharField('Название встречи', max_length=100)
+    date = models.DateField('Дата проведения')
 
-	date = models.DateField('Дата проведения') 
+    def __str__(self):
+        return self.meeting_name
 
-
-	class Meta():
-		verbose_name = 'Встреча'
-		verbose_name_plural = 'Встречи'
-
-
-
+    class Meta():
+        ordering = ['meeting_name', '-date']
+        verbose_name = 'Встреча'
+        verbose_name_plural = 'Встречи'
 
 
 class Person(models.Model):
-	telegram_id = models.CharField('Telegram Id', max_length = 100, null = False)
-	tests_score = models.IntegerField('Очки тестирования', default = 0)
-	competion_score = models.IntegerField('Очки соревнования', default = 0)
-	visited_meetings = models.ManyToManyField(Meeting, related_name = 'people')
+    telegram_id = models.CharField('Telegram ID', max_length=100, null=False)
+    tests_score = models.IntegerField('Очки тестирования', default=0)
+    competition_score = models.IntegerField('Очки соревнования', default=0)
+    visited_meetings = models.ManyToManyField(Meeting, related_name='people',verbose_name='Посещенные встречи')
+    prefered_categories = models.ManyToManyField(
+        Category, related_name='prefered_category',
+        verbose_name='Предпочитаемые категории'
+    )
 
+    def __str__(self):
+        return self.telegram_id
 
-	class Meta():
-		verbose_name = 'Человек'
-		verbose_name_plural = 'Люди'
+    class Meta():
+        verbose_name = 'Человек'
+        verbose_name_plural = 'Люди'
+        ordering = ['-competition_score', '-tests_score']
